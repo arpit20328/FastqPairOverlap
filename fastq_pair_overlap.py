55dfdd31898415f776783eb75daecd19aeb73b37
ARPIT MATHUR <78298300+arpit20328@users.noreply.github.com>
2025-02-06 14:23:58 +0530

    Update fastq_pair_overlap.py

--- fastq_pair_overlap.py
+++ fastq_pair_overlap.py
@@ -1,1 +1,17 @@
+import argparse
 
+def parse_fastq(file1, file2):
+    # Implement your parsing logic here
+    pass
+
+def main():
+    parser = argparse.ArgumentParser(description='Process paired-end FASTQ files.')
+    parser.add_argument('file1', type=str, help='Path to the first FASTQ file')
+    parser.add_argument('file2', type=str, help='Path to the second FASTQ file')
+    
+    args = parser.parse_args()
+    
+    parse_fastq(args.file1, args.file2)
+
+if __name__ == "__main__":
+    main()
